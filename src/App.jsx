18700e349e10507848c412db169cab557877a52e
AlejoMontuori <Alejomontuori12@gmail.com>
2025-09-02 17:58:56 -0300

    agregado de imagenes y section productos

--- src/App.jsx
+++ src/App.jsx
@@ -42,13 +42,29 @@ function App() {
         </header>
 
         <main>
-          <section className="pt-50 pb-24">
-            <div className="flex flex-col space-y-4 w-full px-6 lg:max-w-4xl">
+          <section className="flex justify-center py-24 px-6">
+            <div className="border-4 border-black flex justify-start max-w-5xl w-full">
               <div>
-                <h1 className="text-4xl font-semibold">Sobre Nosotros</h1>
+                <h1>Productos</h1>
+                <div>
+                  <div>
+                    <h2>Combos y Ofertas</h2>
+                  </div>
+                  <div>
+                    <h2>Unidades</h2>
+                  </div>
+                </div>
               </div>
-              <div>
-                <p className="text-2xl">
+            </div>
+          </section>
+
+          <section className="flex justify-center py-24 px-6">
+            {/* Contenedor principal que centra el contenido y maneja la responsividad */}
+            <div className="flex flex-col md:flex-row items-center gap-12 lg:gap-16 max-w-5xl w-full">
+              {/* Contenedor para el texto */}
+              <div className="flex flex-col gap-4 text-center md:text-left">
+                <h1 className="text-4xl font-semibold">Sobre Nosotros</h1>
+                <p className="text-xl">
                   En BazarAyB, nos dedicamos a ofrecer una amplia variedad de
                   productos de alta calidad a precios competitivos. Nuestra
                   misión es brindar a nuestros clientes una experiencia de
@@ -56,11 +72,23 @@ function App() {
                   cliente y la innovación constante.
                 </p>
               </div>
+
+              {/* Contenedor para las imágenes */}
+              <div className="flex flex-shrink-0 gap-4">
+                {/* - w-48 h-48 equivale aprox. a 12rem x 12rem (cerca de 5cm).
+               - object-cover evita que la imagen se deforme.*/}
+                <img
+                  src="/belenPerfil.webp"
+                  alt="Integrante Belén"
+                  className="w-48 h-48 object-cover rounded-lg shadow-md"
+                />
+                <img
+                  src="/alejoPerfil.webp"
+                  alt="Integrante Alejo"
+                  className="w-48 h-48 object-cover rounded-lg shadow-md"
+                />
+              </div>
             </div>
-          </section>
-
-          <section>
-
           </section>
         </main>
       </div>
