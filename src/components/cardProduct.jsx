153c630530243256ea0ba396d065481732087bce
AlejoMontuori <Alejomontuori12@gmail.com>
2025-09-15 16:13:35 -0300

    modificacion de detalles en cardProduct

--- src/components/cardProduct.jsx
+++ src/components/cardProduct.jsx
@@ -38,7 +38,7 @@ const CardProduct = ({ producto }) => {
         )}
 
         <h3
-          className="text-lg font-semibold text-gray-800 mb-2 truncate"
+          className="text-lg font-semibold text-gray-800 mb-2"
           title={nombre}
         >
           {nombre}
